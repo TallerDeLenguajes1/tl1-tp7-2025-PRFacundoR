bb8978713b428a01548154ebc9114f2ded9ed242
FacundoPeraltaRomero <facundoperalta217@gmail.com>
2025-05-27 15:37:28 -0300

    calculadora hecha

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -1,2 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using proyecto1;
+
 Console.WriteLine("Hello, World!");
+
+
+Calculadora calculadora = new Calculadora();
+
+string Opcion,b;
+
+do
+{
+    Console.WriteLine("Ingrese una de las siguientes operaciones, sumar, restar, dividir, multiplicar, o limpiar para limpiar el resultado si quiere salir escriba salir");
+    Opcion = Console.ReadLine();
+
+    Console.WriteLine("ingrese un numero");
+    b = Console.ReadLine();
+
+    if (double.TryParse(b, out double a))
+    {
+        switch (Opcion)
+        {
+            case "sumar":
+                calculadora.Sumar(a);
+                break;
+
+            case "restar":
+                calculadora.Restar(a);
+                break;
+
+            case "multiplicar":
+                calculadora.Multiplicar(a);
+                break;
+
+            case "dividir":
+                calculadora.Dividir(a);
+                break;
+                
+            case "limpiar":
+                calculadora.Limpiar();
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("ingrese un numero, no una letra");
+    }
+
+    Console.WriteLine("El resultado de la " + Opcion + " es " + calculadora.Resultado);
+
+} while (Opcion != "salir");
---
+++ proyecto1/calculadora.cs
@@ -0,0 +1,49 @@
+using System.Reflection;
+
+namespace proyecto1
+{
+   public class Calculadora
+    {
+        private float a, b;//campos =atributos privados, atributos son variables declaradas, metodos publicos
+        private double resultado=0;
+        
+    
+
+        public double Resultado
+        {
+            get => resultado;//leer set es escribir
+        }
+
+        public void Sumar(double termino)
+        {
+           resultado = termino + resultado;
+            
+            
+        }
+        public void Restar(double termino)
+        {
+            resultado -= termino;
+        }
+        public void Multiplicar(double termino)
+        {
+            resultado *= termino;
+        }
+        public void Dividir(double termino)
+        {
+            if (termino != 0)
+            {
+                 resultado /= termino;
+            }
+            else
+            {
+                Console.WriteLine("NO se puede dividir en cero");
+            }
+        
+        }
+        public void Limpiar()
+        {
+             resultado = 0;
+        }
+
+    }
+}
